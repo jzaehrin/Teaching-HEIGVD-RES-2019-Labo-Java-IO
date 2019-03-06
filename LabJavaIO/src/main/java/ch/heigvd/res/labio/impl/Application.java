a177c35fe3cba92f813978274a82d912b0327f88
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-03-06 10:21:41 +0100

    Try implement Application

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,8 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -91,6 +89,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote, String.format("quote-%d", i));
 
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
@@ -125,7 +124,19 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String path = WORKSPACE_DIRECTORY;
+
+    for(String tag : quote.getTags()){
+      path = String.format("%s/%s", path, tag);
+    }
+
+    path = String.format("%s/%s.utf8", path, filename);
+
+    File file = new File(path);
+    file.getParentFile().mkdirs();
+
+    FileWriter fw = new FileWriter(file);
+    fw.write(quote.getQuote());
   }
   
   /**
@@ -142,6 +153,11 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try {
+          writer.write(file.getPath() + file.getName());
+        } catch (IOException e) {
+          LOG.severe("Received an IO error in printFileNames");
+        }
       }
     });
   }
